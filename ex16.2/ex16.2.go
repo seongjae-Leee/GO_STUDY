c14727b7ec082053f31bd254de6f0b05f8a1cee6
Seongjae <robbyl95@hanmail.net>
2021-12-06 17:02:44 +0900

    Go 모듈 만들고 외부 패키지 활용하기

--- ex16.2/ex16.2.go
+++ ex16.2/ex16.2.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/rand"
 	_ "strings" // 불러와야 하는 경우, 그러나 쓰지는 않는 경우 별칭을 줘서 쓸 수 있고, _ 를 쓰게 되면 무명으로 불러올 수 있게 된다.
+	// ★왜 사용하지 않는 패키지를 포함해야 하는가? -> 패키지 초기화에 따른 부가효과를 위해서.
 )
 
 func main() {
@@ -13,3 +14,14 @@ func main() {
 }
 
 // 결과값 : 5577006791947779410
+
+/*
+Go 모듈 만들고 외부 패키지 활용하기
+1. goproject/usepkg 폴더 생성
+2. go mod init goproject/usepkg
+3. mkdir custompkg
+4. Custompkg.go
+5. mkdir program
+6. usepkg.go
+7. go mod tidy
+*/
