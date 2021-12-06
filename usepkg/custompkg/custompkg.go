b3cb01c09677d51cd5f7d54dd70d9f63f3e9a795
Seongjae <robbyl95@hanmail.net>
2021-12-06 21:29:30 +0900

    패키지 외부 공개

--- usepkg/custompkg/custompkg.go
+++ usepkg/custompkg/custompkg.go
@@ -2,6 +2,7 @@ package custompkg
 
 import "fmt"
 
+// 아래와 같이 함수 이름이 대문자로 시작하면 다른 파일에서도 import해서 쓸 수 있지만 소문자로 되어있으면 export가 안되어서 불러다 쓸 수 없음
 func PrintCustom() {
 	fmt.Println("This is custom pakcage~!!")
 }
