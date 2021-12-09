03c4973e1708371a571533f177799b790d6859e7
Seongjae <robbyl95@hanmail.net>
2021-12-10 02:01:29 +0900

    스택

--- ex22.3/ex22.3.go
+++ ex22.3/ex22.3.go
@@ -7,37 +7,36 @@ import (
 	"fmt"
 )
 
-type Stack struct {
+type Queue struct { // ❶ Queue 구조체 정의
 	v *list.List
 }
 
-func NewStack() *Stack {
-	return &Stack{list.New()}
+func (q *Queue) Push(val interface{}) { // ❷ 요소 추가
+	q.v.PushBack(val)
 }
 
-func (s *Stack) Push(val interface{}) {
-	s.v.PushBack(val) // ❶ 맨 뒤에 요소 추가
-}
-
-func (s *Stack) Pop() interface{} {
-	back := s.v.Back() // ❷ 맨 뒤에서 요소를 반환
-	if back != nil {
-		return s.v.Remove(back)
+func (q *Queue) Pop() interface{} { // ❸ 요소을 반환하면서 삭제
+	front := q.v.Front()
+	if front != nil {
+		return q.v.Remove(front)
 	}
 	return nil
 }
 
+func NewQueue() *Queue {
+	return &Queue{list.New()}
+}
+
 func main() {
-	stack := NewStack()
+	queue := NewQueue() // ❹ 새로운 큐 생성
 
-	for i := 1; i < 5; i++ {
-		stack.Push(i)
+	for i := 1; i < 5; i++ { // ➎ 요소 입력
+		queue.Push(i)
 	}
-
-	val := stack.Pop()
-	for val != nil {
-		fmt.Printf("%v -> ", val)
-		val = stack.Pop()
+	v := queue.Pop()
+	for v != nil { // ➏ 요소 출력
+		fmt.Printf("%v -> ", v)
+		v = queue.Pop()
 	}
 }
 
